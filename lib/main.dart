796ccca48c6e3eaaf5c300acf44f5a5197584665
alimasudd <alimasudd.dev@gmail.com>
2024-05-21 08:28:51 +0700

    add FirebaseAuth.instance.authStateChanges in main.dart to switch between home and login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:catat_kendaraan/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -13,10 +14,27 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
+
+    StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            );
+          }
+          return GetMaterialApp(
+            title: "Application",
+            initialRoute: snapshot.data !=null ? Routes.HOME : Routes.LOGIN,
+            getPages: AppPages.routes,
+          );
+        }
+
+
+        )
   );
 }
