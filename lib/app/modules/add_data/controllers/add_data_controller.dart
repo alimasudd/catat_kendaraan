4fc31f101bd29d6ced0d3ac264eb0c49de0d21a5
alimasudd <alimasudd.dev@gmail.com>
2024-05-28 16:21:49 +0700

    create function pickdate

--- lib/app/modules/add_data/controllers/add_data_controller.dart
+++ lib/app/modules/add_data/controllers/add_data_controller.dart
@@ -15,5 +15,31 @@ class AddDataController extends GetxController {
   final TFLocation = TextEditingController();
   final TFKronologi = TextEditingController();
 
+  void pickDate() async {
+    DateTime? dateTime = await showDatePicker(
+        context: Get.context as BuildContext,
+        initialDate: DateTime.now(),
+        builder: (context, child) {
+          return Theme(
+            data: Theme.of(context).copyWith(
+              colorScheme: const ColorScheme.light(
+                primary: AppColors.primary, // <-- SEE HERE
+              ),
+            ),
+            child: child!,
+          );
+        },
+        firstDate: DateTime(1980),
+        //DateTime.now() - not to allow to choose before today.
+        lastDate: DateTime.now()
+    );
+
+    if (dateTime != null) {
+      String formattedDate = DateFormat('yyyy-MM-dd').format(dateTime);
+      //you can implement different kind of Date Format here according to your requirement
+      TFDate.text = formattedDate;
+    }
+  }
+
 
 }
