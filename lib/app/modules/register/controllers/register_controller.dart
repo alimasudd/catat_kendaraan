9bcaddf7d38a09573f91dfc1fbe5ba1bfd4172bc
alimasudd <alimasudd.dev@gmail.com>
2024-05-28 16:24:39 +0700

    add function register to send data to firebase

--- lib/app/modules/register/controllers/register_controller.dart
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -14,4 +14,42 @@ class RegisterController extends GetxController {
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  void register() async {
+    if (TFName.text.isNotEmpty && TFEmail.text.isNotEmpty && TFPassword.text.isNotEmpty) {
+      try {
+        UserCredential credential = await auth.createUserWithEmailAndPassword(
+            email: TFEmail.text,
+            password: TFPassword.text
+        );
+        if(credential.user != null){
+          String uid = credential.user!.uid;
+
+          await firestore.collection('users').doc(uid).set({
+            "uid" : uid,
+            "name" : TFName.text,
+            "email" : TFEmail.text,
+            "created_at" : DateTime.now().toIso8601String(),
+          });
+          await credential.user!.sendEmailVerification();
+        }
+
+        AlertDialogInfo.showAlertDialog(Get.context as BuildContext, true, 'Informasi', 'Email verifikasi sudah terkirim. Silahkan verifikasi email anda', 'OKE', () {
+          Get.back();
+        });
+
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          Get.snackbar('Terjadi Kesalahan', 'Email sudah pernah didaftarkan',
+              backgroundColor: AppColors.primary, colorText: AppColors.white, snackPosition: SnackPosition.BOTTOM);
+        } else if (e.code == 'weak-password') {
+          Get.snackbar('Terjadi Kesalahan', 'Password yang digunakan terlalu lemah',
+            backgroundColor: AppColors.primary, colorText: AppColors.white, snackPosition: SnackPosition.BOTTOM);
+        }
+      }
+    } else {
+      AlertDialogInfo.showAlertDialog(Get.context as BuildContext, true, 'Peringatan', 'Kolom tidak boleh kosong', 'OKE', () {
+        Get.back();
+      });
+    }
+  }
 }
