d95df6857204ca0e91eb2ea2303291934c039bbf
alimasudd <alimasudd.dev@gmail.com>
2024-05-29 16:52:40 +0700

    modified TextButton widget_alert_dialog.dart

--- lib/app/widgets/widget_alert_dialog.dart
+++ lib/app/widgets/widget_alert_dialog.dart
@@ -13,19 +13,34 @@ class AlertDialogWidget{
 
     // set up the buttons
     Widget cancelButton = TextButton(
-      child: const Text("Batal", style: TextStyle(fontSize: 14, color: AppColors.grey),),
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.grey.shade700,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8)),
+        side: const BorderSide(
+            width: 2, color: AppColors.grey),
+      ),
+      child: Text("Batal", style: TextStyle(fontSize: 13, color: Colors.grey.shade700),),
       onPressed:  () => Navigator.of(context, rootNavigator: true).pop(),
     );
+
     Widget continueButton = TextButton(
         onPressed:  onPressed,
+      style: TextButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8)),
+        side: const BorderSide(
+            width: 2, color: AppColors.primary),
+      ),
         child: Text(buttonText,
-          style: const TextStyle(fontSize: 14, color: AppColors.primary),)
+          style: const TextStyle(fontSize: 13, color: AppColors.white),)
     );
 
     AlertDialog alert = AlertDialog(
       surfaceTintColor: Colors.white,
       shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(7)),
-      title: Text(title),
+      title: Text(title, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
       content: Text(content),
       actions: [
         cancelButton,
