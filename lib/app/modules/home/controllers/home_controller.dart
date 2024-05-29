140007a468e944519d0c193f0423459e3e046cc6
alimasudd <alimasudd.dev@gmail.com>
2024-05-29 16:53:38 +0700

    add streamData in home_controller.dart

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,11 +1,20 @@
+import 'package:catat_kendaraan/app/data/data_model.dart';
+import 'package:catat_kendaraan/app/routes/app_pages.dart';
+import 'package:catat_kendaraan/app/widgets/widget_alert_dialog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
 
-  final count = 0.obs;
+  var YTid = '';
 
-
-
-  void increment() => count.value++;
+  Stream<List<DataModel>> streamData() => FirebaseFirestore.instance
+      .collection('data')
+      .orderBy("date", descending: true)
+      .snapshots()
+      .map((snapshot) =>
+      snapshot.docs.map((e) => DataModel.fromJson(e.data())).toList());
 }
