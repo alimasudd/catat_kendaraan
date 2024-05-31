979736b082e51f5e182478ed7b6fde3c155562d4
alimasudd <alimasudd.dev@gmail.com>
2024-05-31 09:33:17 +0700

    modified app_pages.dart

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -17,4 +17,42 @@ class HomeController extends GetxController {
       .snapshots()
       .map((snapshot) =>
       snapshot.docs.map((e) => DataModel.fromJson(e.data())).toList());
+
+  void logout() {
+    AlertDialogWidget.showAlertDialog(Get.context as BuildContext,
+        'Keluar', 'Anda yakin ingin keluar', 'Keluar', () async {
+        await FirebaseAuth.instance.signOut();
+        Get.offAllNamed(Routes.LOGIN);
+        });
+  }
+
+  Stream<List<DataModel>> streamDataByUserId() {
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+    return FirebaseFirestore.instance
+        .collection('data')
+        .where('user_id', isEqualTo: userId)
+        .snapshots()
+        .map((QuerySnapshot query) {
+      List<DataModel> dataList = [];
+      for (var doc in query.docs) {
+        dataList.add(DataModel.fromDocument(doc));
+      }
+      return dataList;
+    });
+  }
+
+  void updateData(id, views) async {
+    try{
+      int currentViews = views;
+      currentViews += 1;
+      final docData = FirebaseFirestore.instance
+          .collection('data')
+          .doc(id);
+      await docData.update({
+        'views' : currentViews
+      });
+    }on FirebaseException catch(e){
+      debugPrint('firebaseError $e');
+    }
+  }
 }
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,10 @@
+import 'package:catat_kendaraan/app/modules/bottom_menu/bottom_menu.dart';
 import 'package:get/get.dart';
 
+import '../modules/add_data/bindings/add_data_binding.dart';
+import '../modules/add_data/views/add_data_view.dart';
+import '../modules/detail_data/bindings/detail_data_binding.dart';
+import '../modules/detail_data/views/detail_data_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
@@ -16,6 +21,10 @@ class AppPages {
 
   static final routes = [
     GetPage(
+      name: _Paths.MENU,
+      page: () => const BottomMenu(),
+    ),
+    GetPage(
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
@@ -29,6 +38,16 @@ class AppPages {
       name: _Paths.LOGIN,
       page: () => const LoginView(),
       binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.ADD_DATA,
+      page: () => const AddDataView(),
+      binding: AddDataBinding(),
+    ),
+    GetPage(
+      name: _Paths.DETAIL_DATA,
+      page: () => const DetailDataView(),
+      binding: DetailDataBinding(),
     ),
   ];
 }
