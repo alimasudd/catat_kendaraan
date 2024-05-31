baeebca5ad9856ddd133c26ae414706536d983b2
alimasudd <alimasudd.dev@gmail.com>
2024-05-31 09:36:19 +0700

    modified view home_view.dart

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:catat_kendaraan/app/modules/home/child/home_child.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -5,18 +6,44 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: StreamBuilder(
+          stream: controller.streamData(),
+          builder: (context, snapshot){
+            if(snapshot.hasError){
+              return Text('${snapshot.error}');
+            }else if(snapshot.hasData){
+              final records = snapshot.data!;
+
+              return ListView.builder(
+                  shrinkWrap: true,
+                  padding: EdgeInsets.zero,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: records.length,
+                      // > 1 ? 2 : records.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return HomeChild(
+                      id: records[index].id,
+                      urlImage: records[index].urlYoutube,
+                      location: records[index].location,
+                      plat: records[index].platNumber,
+                      date: records[index].date,
+                      views: records[index].views,
+                    );
+                  });
+              // return ListView(
+              //   children: records.map(controller.buildRecord).toList(),
+              // );
+            }else{
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
         ),
       ),
     );
