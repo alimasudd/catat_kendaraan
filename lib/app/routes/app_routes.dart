3a4b47a088ccb1ca549f1ff5728a8d9e36597027
alimasudd <alimasudd.dev@gmail.com>
2024-05-31 09:33:28 +0700

    modified app_routes.dart

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,14 +3,20 @@ part of 'app_pages.dart';
 
 abstract class Routes {
   Routes._();
+  static const MENU = _Paths.MENU;
   static const HOME = _Paths.HOME;
   static const REGISTER = _Paths.REGISTER;
   static const LOGIN = _Paths.LOGIN;
+  static const ADD_DATA = _Paths.ADD_DATA;
+  static const DETAIL_DATA = _Paths.DETAIL_DATA;
 }
 
 abstract class _Paths {
   _Paths._();
+  static const MENU = '/menu';
   static const HOME = '/home';
   static const REGISTER = '/register';
   static const LOGIN = '/login';
+  static const ADD_DATA = '/add-data';
+  static const DETAIL_DATA = '/detail-data';
 }
